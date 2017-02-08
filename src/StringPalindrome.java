2090fcd5e54509e3deabfe9ec5537ba94a2c7db4
Hanna Zhahala <Hanna_Zhahala@epam.com>
2017-02-08 09:55:45 +0100

    Palindrome

---
+++ src/StringPalindrome.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Hanna_Zhahala on 07.02.2017.
+ */
+import java.util.Scanner;
+public class StringPalindrome {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("YOUR STRING is: ");
+        String a = in.nextLine();
+
+        String reversedString = new StringBuffer(a).reverse().toString();
+        System.out.println(reversedString);
+        System.out.println("THE REVERSED " +
+                "STRING IS: " + reversedString);
+
+
+
+        if (a.equals(reversedString))
+            System.out.println("THIS IS A PALINDROME");
+        else
+            System.out.println("THIS IS NOT A PALINDROME");
+    }
+}
+
+
